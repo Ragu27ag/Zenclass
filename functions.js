84065ea3c82bbcab4c7359141036859a6abc0059
ragunath <raguyuvi27@gmail.com>
2022-12-31 11:00:22 +0530

    initial

---
+++ functions.js
@@ -0,0 +1,29 @@
+var a = [1,2,3,4,2];
+// console.log(a.slice(0,3));
+// console.log(a);
+
+// console.log(a.splice(0,2));
+// console.log(a);
+
+
+// var b = a.map((e) => e+=e);
+
+// console.log(b);
+
+// var c = a.find((e) => e = 1)
+// console.log(c)
+
+// var c = a.filter((e) => e == 5)
+// console.log(c)
+
+// var c = a.reduce((r,a) => {
+//     return r+a;
+// },10);
+// console.log(c);
+
+console.log(a.lastIndexOf(2)); 
+
+var b = 'abcd'
+
+console.log(a.join('/'));
+console.log(b.split('',1))
